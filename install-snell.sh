c8e6cebc493478584d603f9a60eff5bf7304dec4
Simondler <global900923@gmail.com>
2024-12-09 10:06:51 +0800

    1

--- install-snell.sh
+++ install-snell.sh
@@ -76,11 +76,9 @@ else
 fi
 
 # 安装bbr
-
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
-lsmod | grep bbr
 
 if lsmod | grep bbr; then
    echo "BBR安装成功"
