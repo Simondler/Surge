af8f2918225a0929d8fc5af0898be8b26a0e77d1
Simondler <global900923@gmail.com>
2024-12-12 03:44:01 +0800

    1

--- beautify_prompt.sh
+++ beautify_prompt.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # 定义 ANSI 颜色代码
-GREEN="\033[1;32m"
-BLUE="\033[1;34m"
-RED="\033[1;31m"
-MAGENTA="\033[1;35m"
-CYAN="\033[1;36m"
-YELLOW="\033[1;33m"
-WHITE="\033[1;37m"
+BLACK="\033[0;30m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
+BRIGHT_BLACK="\033[1;30m"
+BRIGHT_RED="\033[1;31m"
+BRIGHT_GREEN="\033[1;32m"
+BRIGHT_YELLOW="\033[1;33m"
+BRIGHT_BLUE="\033[1;34m"
+BRIGHT_MAGENTA="\033[1;35m"
+BRIGHT_CYAN="\033[1;36m"
+BRIGHT_WHITE="\033[1;37m"
 RESET="\033[0m"
 
 # 自动判断使用的是 bash 还是 zsh
@@ -22,17 +31,23 @@ fi
 
 # 显示选项
 echo -e "请选择您喜欢的命令行提示符样式:"
-echo -e "1. ${GREEN}\u@${BLUE}\h:${RED}\w${RESET}#"
-echo -e "2. ${MAGENTA}\u@${CYAN}\h:${YELLOW}\w${RESET}#"
-echo -e "3. ${RED}\u@${GREEN}\h:${BLUE}\w${RESET}#"
-echo -e "4. ${CYAN}\u@${YELLOW}\h:${WHITE}\w${RESET}#"
-echo -e "5. ${WHITE}\u@${RED}\h:${GREEN}\w${RESET}#"
-echo -e "6. ${YELLOW}\u@${BLUE}\h:${MAGENTA}\w${RESET}#"
-echo -e "7. ${BLUE}\u@${MAGENTA}\h:${CYAN}\w${RESET}#"
-echo -e "8. ${CYAN}\u@${RED}\h:${WHITE}\w${RESET}#"
-echo -e "9. ${MAGENTA}\u@${GREEN}\h:${YELLOW}\w${RESET}#"
-echo -e "10. ${YELLOW}\u@${CYAN}\h:${BLUE}\w${RESET}#"
-echo -e "11. 默认样式：\u@\h:\w#"
+echo -e "1. ${RED}\u@${GREEN}\h:${BLUE}\w${RESET}#"
+echo -e "2. ${YELLOW}\u@${MAGENTA}\h:${CYAN}\w${RESET}#"
+echo -e "3. ${BRIGHT_RED}\u@${BRIGHT_GREEN}\h:${BRIGHT_BLUE}\w${RESET}#"
+echo -e "4. ${BRIGHT_YELLOW}\u@${BRIGHT_MAGENTA}\h:${BRIGHT_CYAN}\w${RESET}#"
+echo -e "5. ${BLACK}\u@${BRIGHT_RED}\h:${BRIGHT_WHITE}\w${RESET}#"
+echo -e "6. ${BRIGHT_BLACK}\u@${WHITE}\h:${GREEN}\w${RESET}#"
+echo -e "7. ${BLUE}\u@${BRIGHT_YELLOW}\h:${RED}\w${RESET}#"
+echo -e "8. ${BRIGHT_BLUE}\u@${MAGENTA}\h:${CYAN}\w${RESET}#"
+echo -e "9. ${CYAN}\u@${YELLOW}\h:${MAGENTA}\w${RESET}#"
+echo -e "10. ${MAGENTA}\u@${BRIGHT_CYAN}\h:${BRIGHT_YELLOW}\w${RESET}#"
+echo -e "11. ${WHITE}\u@${BRIGHT_BLACK}\h:${BRIGHT_RED}\w${RESET}#"
+echo -e "12. ${BRIGHT_WHITE}\u@${BRIGHT_BLUE}\h:${BRIGHT_GREEN}\w${RESET}#"
+echo -e "13. ${GREEN}\u@${CYAN}\h:${BRIGHT_MAGENTA}\w${RESET}#"
+echo -e "14. ${BRIGHT_YELLOW}\u@${BRIGHT_RED}\h:${BRIGHT_BLACK}\w${RESET}#"
+echo -e "15. ${BRIGHT_GREEN}\u@${BLUE}\h:${BLACK}\w${RESET}#"
+echo -e "16. ${BRIGHT_MAGENTA}\u@${RED}\h:${BRIGHT_CYAN}\w${RESET}#"
+echo -e "17. 默认样式：\u@\h:\w#"
 
 # 提示用户输入选择
 read -p "请输入对应样式的数字： " choice
@@ -40,36 +55,54 @@ read -p "请输入对应样式的数字： " choice
 # 根据选择生成 PS1
 case $choice in
     1)
-        PS1="${GREEN}\u@${BLUE}\h:${RED}\w${RESET}#"
-        ;;
-    2)
-        PS1="${MAGENTA}\u@${CYAN}\h:${YELLOW}\w${RESET}#"
-        ;;
-    3)
         PS1="${RED}\u@${GREEN}\h:${BLUE}\w${RESET}#"
         ;;
+    2)
+        PS1="${YELLOW}\u@${MAGENTA}\h:${CYAN}\w${RESET}#"
+        ;;
+    3)
+        PS1="${BRIGHT_RED}\u@${BRIGHT_GREEN}\h:${BRIGHT_BLUE}\w${RESET}#"
+        ;;
     4)
-        PS1="${CYAN}\u@${YELLOW}\h:${WHITE}\w${RESET}#"
+        PS1="${BRIGHT_YELLOW}\u@${BRIGHT_MAGENTA}\h:${BRIGHT_CYAN}\w${RESET}#"
         ;;
     5)
-        PS1="${WHITE}\u@${RED}\h:${GREEN}\w${RESET}#"
+        PS1="${BLACK}\u@${BRIGHT_RED}\h:${BRIGHT_WHITE}\w${RESET}#"
         ;;
     6)
-        PS1="${YELLOW}\u@${BLUE}\h:${MAGENTA}\w${RESET}#"
+        PS1="${BRIGHT_BLACK}\u@${WHITE}\h:${GREEN}\w${RESET}#"
         ;;
     7)
-        PS1="${BLUE}\u@${MAGENTA}\h:${CYAN}\w${RESET}#"
+        PS1="${BLUE}\u@${BRIGHT_YELLOW}\h:${RED}\w${RESET}#"
         ;;
     8)
-        PS1="${CYAN}\u@${RED}\h:${WHITE}\w${RESET}#"
+        PS1="${BRIGHT_BLUE}\u@${MAGENTA}\h:${CYAN}\w${RESET}#"
         ;;
     9)
-        PS1="${MAGENTA}\u@${GREEN}\h:${YELLOW}\w${RESET}#"
+        PS1="${CYAN}\u@${YELLOW}\h:${MAGENTA}\w${RESET}#"
         ;;
     10)
-        PS1="${YELLOW}\u@${CYAN}\h:${BLUE}\w${RESET}#"
+        PS1="${MAGENTA}\u@${BRIGHT_CYAN}\h:${BRIGHT_YELLOW}\w${RESET}#"
         ;;
     11)
+        PS1="${WHITE}\u@${BRIGHT_BLACK}\h:${BRIGHT_RED}\w${RESET}#"
+        ;;
+    12)
+        PS1="${BRIGHT_WHITE}\u@${BRIGHT_BLUE}\h:${BRIGHT_GREEN}\w${RESET}#"
+        ;;
+    13)
+        PS1="${GREEN}\u@${CYAN}\h:${BRIGHT_MAGENTA}\w${RESET}#"
+        ;;
+    14)
+        PS1="${BRIGHT_YELLOW}\u@${BRIGHT_RED}\h:${BRIGHT_BLACK}\w${RESET}#"
+        ;;
+    15)
+        PS1="${BRIGHT_GREEN}\u@${BLUE}\h:${BLACK}\w${RESET}#"
+        ;;
+    16)
+        PS1="${BRIGHT_MAGENTA}\u@${RED}\h:${BRIGHT_CYAN}\w${RESET}#"
+        ;;
+    17)
         PS1="\u@\h:\w#"
         ;;
     *)
@@ -79,7 +112,7 @@ case $choice in
 esac
 
 # 将新的 PS1 写入配置文件
-echo -e "\n# 自定义命令行提示符（去掉 ~ 和 # 之间的空格）" >> "$CONFIG_FILE"
+echo -e "\n# 自定义命令行提示符（支持 16 种颜色）" >> "$CONFIG_FILE"
 echo "export PS1=\"${PS1}\"" >> "$CONFIG_FILE"
 
 # 加载配置文件
